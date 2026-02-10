abc8d7278fce56de1360a0445afe492f6565f555
garmartirosy <76405829+garmartirosy@users.noreply.github.com>
2026-02-09 19:10:41 -0500

    Delete Personal Finance Manager/Observers/TransactionChangedEvent.cs

--- Personal Finance Manager/Observers/TransactionChangedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Personal_Finance_Manager.Observers;
-
-public record TransactionChangedEvent(
-    string OwnerUserId,
-    int TransactionId,
-    int? AccountId,
-    string Action,       
-    DateTime Time
-);
